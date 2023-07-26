b6a2a77927b81139db958185e382c77c79663662
mashoun <mashoun.me@gmail.com>
2023-07-26 21:51:24 +0300

    *navbar *image ratio

--- js/components/navbar/navbar.js
+++ js/components/navbar/navbar.js
@@ -10,28 +10,26 @@ export default {
         }
     },
     mounted() {
-        if (location.href.includes('/blogs') || location.href.includes('/team')|| location.href.includes('/privacy-policy') || location.href.includes('/terms')) {
-            // document.querySelector('header').classList.add('bg-light')
-            // document.querySelector('header').classList.remove('text-light')
+        // if (location.href.includes('/blogs') || location.href.includes('/team')|| location.href.includes('/privacy-policy') || location.href.includes('/terms')) {
+        //     // document.querySelector('header').classList.add('bg-light')
+        //     // document.querySelector('header').classList.remove('text-light')
 
-            // document.querySelector('icon-bar').classList.add('bg-light')
-            // document.querySelector('icon-bar').classList.remove('text-light')
-        }
+        //     // document.querySelector('icon-bar').classList.add('bg-light')
+        //     // document.querySelector('icon-bar').classList.remove('text-light')
+        // }
 
         document.addEventListener('scroll',e => {
             // console.log(window.scrollY);
-            if(window.scrollY >= 10) {
-                // document.querySelector('header').classList.add('bg-light')
-                // document.querySelector('header').classList.remove('text-light')
-
-                // document.querySelector('icon-bar').classList.add('bg-light')
-                document.querySelector('#icon-bar').classList.remove('text-light')
-            }else {
-                // document.querySelector('header').classList.remove('bg-light')
-                // document.querySelector('header').classList.add('text-light')
-
-                // document.querySelector('icon-bar').classList.remove('bg-light')
-                document.querySelector('#icon-bar').classList.add('text-light')
+            if(window.scrollY >= 1) {
+                document.querySelector('header').classList.add('position-fixed')
+                document.querySelector('header').classList.add('top-0')
+                document.querySelector('header').classList.add('start-0')
+                document.querySelector('header').classList.add('z-2')
+            }else {                
+                document.querySelector('header').classList.remove('position-fixed')
+                document.querySelector('header').classList.remove('top-0')
+                document.querySelector('header').classList.remove('start-0')
+                document.querySelector('header').classList.remove('z-2')
             }
         })
 
