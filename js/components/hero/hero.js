764d6192648938690efe6f247bb8ca1482c548ca
mashoun <mashoun.me@gmail.com>
2024-01-11 21:54:19 +0200

    updated hero img src https://lh3.googleusercontent.com/d/

--- js/components/hero/hero.js
+++ js/components/hero/hero.js
@@ -8,7 +8,7 @@ export default {
             utilities,
             slides:[
                 // './img/hero.png',
-                'https://drive.google.com/uc?id=14eMqHX0HhC6r2MlCxWlajHtz9-qNj7-i'
+                'https://lh3.googleusercontent.com/d/14eMqHX0HhC6r2MlCxWlajHtz9-qNj7-i'
                 // 'https://image.lexica.art/full_jpg/bfc6af3f-4473-4a78-8543-ee98f74361b3',
                 // 'https://image.lexica.art/full_jpg/9af93040-d816-43c8-9e74-989e7b10f346',
                 // 'https://image.lexica.art/full_jpg/5223e6e4-03d4-454c-b6ae-9bf5f3abc26d',
